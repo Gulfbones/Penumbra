70ac4f551a4c90001d62df83f029921a1247bb18
HonduranCoder42 <mondragonv2997@my.uwstout.edu>
2022-10-26 11:18:35 -0500

    Added small LevelChanger script + box

---
+++ Penumbra_Game/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // To use, enter scene name (pretty simple)
+        //SceneManager.LoadScene();
+    }
+}
