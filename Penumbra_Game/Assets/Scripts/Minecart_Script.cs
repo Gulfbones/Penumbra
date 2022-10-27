d85edf025766ae443cd19c449caee47736a6be72
Aidan Brausen <Terrorturtled@gmail.com>
2022-10-27 02:30:21 -0500

    The cart shakes now

--- Penumbra_Game/Assets/Scripts/Minecart_Script.cs
+++ Penumbra_Game/Assets/Scripts/Minecart_Script.cs
@@ -13,11 +13,18 @@ public class Minecart_Script : MonoBehaviour
     private Rigidbody2D CartPhysicsEngine;
     //private SpriteRenderer spriteRenderer;
 
+    private Vector3 originPosition;
+    private Quaternion originRotation;
+    public float shake_decay = 0.002f;
+    public float shake_intensity = 0.3f;
+
+    private float temp_shake_intensity = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         mainGameObject = gameObject;
-        sprite = GetComponent<SpriteRenderer>();
+        sprite = gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
         //cart = mainGameObject.transform.GetChild(0).gameObject;
         CartPhysicsEngine = GetComponent<Rigidbody2D>();
         Physics2D.IgnoreCollision(GameObject.FindGameObjectWithTag("Enemy").GetComponent<Collider2D>(), GetComponent<Collider2D>());
@@ -30,17 +37,48 @@ public class Minecart_Script : MonoBehaviour
     {
         //Debug.Log("CartPhysicsEngine.velocity.x = " + CartPhysicsEngine.velocity.x);
         //Debug.Log("CartPhysicsEngine.velocity.y = " + CartPhysicsEngine.velocity.y);
-        if (CartPhysicsEngine.velocity.x > 1.0f || CartPhysicsEngine.velocity.x < -1.0f)
+        if (CartPhysicsEngine.velocity.x > 0.01f || CartPhysicsEngine.velocity.x < -0.01f)
+        {
+            shake_intensity = Mathf.Abs(CartPhysicsEngine.velocity.x * 0.012f);
+            Shake();
+        }
+        if (CartPhysicsEngine.velocity.y > 0.01f || CartPhysicsEngine.velocity.y < -0.01f)
+        {
+            shake_intensity = Mathf.Abs(CartPhysicsEngine.velocity.y * 0.012f);
+            Shake();
+        }
+        if (CartPhysicsEngine.velocity.x > 2.0f || CartPhysicsEngine.velocity.x < -2.0f)
         {
             //CartPhysicsEngine.transform.rotation = Quaternion.Euler(0, 0, 0);
             gameObject.transform.GetChild(0).gameObject.transform.rotation = Quaternion.Euler(0, 0, 90);
             sprite.sprite = sideSprite;
+            //temp_shake_intensity = CartPhysicsEngine.velocity.x;
         }
-        if (CartPhysicsEngine.velocity.y > 1.0f || CartPhysicsEngine.velocity.y < -1.0f)
+        if (CartPhysicsEngine.velocity.y > 2.0f || CartPhysicsEngine.velocity.y < -2.0f)
         {
             gameObject.transform.GetChild(0).gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            //gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + (Mathf.Sin(Time.deltaTime * 1.0f) * 1.5f), 0);
+            //gameObject.transform.GetChild(0).gameObject.transform.position = new Vector3(gameObject.transform.GetChild(0).gameObject.transform.position.x, gameObject.transform.GetChild(0).gameObject.transform.position.y + (Mathf.Sin(Time.deltaTime * 1.0f) * 1.5f), 0);
             sprite.sprite = upSprite;
+            //Shake();//temp_shake_intensity = shake_intensity;//temp_shake_intensity = CartPhysicsEngine.velocity.y;
         }
+        if (temp_shake_intensity > 0)
+        {
+            gameObject.transform.GetChild(0).transform.position = originPosition + Random.insideUnitSphere * temp_shake_intensity;
+            gameObject.transform.GetChild(0).transform.rotation = new Quaternion(
+                originRotation.x + Random.Range(-temp_shake_intensity, temp_shake_intensity) * .2f,
+                originRotation.y + Random.Range(-temp_shake_intensity, temp_shake_intensity) * .2f,
+                originRotation.z + Random.Range(-temp_shake_intensity, temp_shake_intensity) * .2f,
+                originRotation.w + Random.Range(-temp_shake_intensity, temp_shake_intensity) * .2f);
+            temp_shake_intensity -= shake_decay;
+        }
+    }
+    void Shake()
+    {
+        originPosition = transform.position;
+        originRotation = transform.rotation;
+        temp_shake_intensity = shake_intensity;
+        Debug.Log("shaka");
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
