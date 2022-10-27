7d5feae7c2cbe97803bee2a97f7f9ecb87725036
HonduranCoder42 <mondragonv2997@my.uwstout.edu>
2022-10-26 22:11:13 -0500

    Update UIManager.cs

--- Penumbra_Game/Assets/Scripts/UIManager.cs
+++ Penumbra_Game/Assets/Scripts/UIManager.cs
@@ -1,10 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject pauseMenu;
 
-    //[SerializeField] private Text _gameOverText;
+    public static bool isPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        if (isPaused)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
 
 }
