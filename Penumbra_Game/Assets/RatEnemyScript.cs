1065aa5f13460057a7d10301811573016a4948bf
Devastator1313 <113194162+Devastator1313@users.noreply.github.com>
2022-11-14 16:29:18 -0600

    added a star to rat

--- Penumbra_Game/Assets/RatEnemyScript.cs
+++ Penumbra_Game/Assets/RatEnemyScript.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class RatEnemyScript : MonoBehaviour
 {
+    private GameObject playerGameObject;
+    private PlayerScript playerScript;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        playerScript = playerGameObject.GetComponent<PlayerScript>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-     if(true/*within attacking range of player*/)
+        /*within attacking range of player*/
+        if (Mathf.Abs(playerGameObject.transform.position.x - playerGameObject.transform.position.x) <= 2 && Mathf.Abs(playerGameObject.transform.position.y - playerGameObject.transform.position.y) <= 2)
         {
+            animator.SetBool("playAttacking", true);
             //play attack animation
             //attack the player
+        }
+        else
+        {
+            animator.SetBool("playAttacking", false);
         }
         
     }
