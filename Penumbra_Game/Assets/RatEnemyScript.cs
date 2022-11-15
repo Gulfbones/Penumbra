d5f0060bc71df57c5449bfa028449d7895a9152e
Devastator1313 <113194162+Devastator1313@users.noreply.github.com>
2022-11-14 23:32:33 -0600

    updated player prefab

--- Penumbra_Game/Assets/RatEnemyScript.cs
+++ Penumbra_Game/Assets/RatEnemyScript.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using Pathfinding;
+
 
 public class RatEnemyScript : MonoBehaviour
 {
     private GameObject playerGameObject;
     private PlayerScript playerScript;
     private Animator animator;
+    private Vector3 desiredScale;
+    public AIPath aiPath;
+    private Vector3 defaultScale;
+    public Vector3 destination;
+    private float moveSpeed;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         playerGameObject = GameObject.FindGameObjectWithTag("Player");
         playerScript = playerGameObject.GetComponent<PlayerScript>();
         animator = gameObject.GetComponent<Animator>();
+        desiredScale = transform.localScale;
+        defaultScale = transform.localScale;
+        moveSpeed = aiPath.maxSpeed;
+
+
+
     }
 
     // Update is called once per frame
@@ -39,6 +57,36 @@ public class RatEnemyScript : MonoBehaviour
         {
             animator.SetBool("moving", false);
         }
-        
+
+        //copied from stalker enemy
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            desiredScale = new Vector3(defaultScale.x, defaultScale.y, defaultScale.z);
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            desiredScale = new Vector3(defaultScale.x * -1, defaultScale.y, defaultScale.z);
+        }
+
+    }
+
+    //copied from stalker enemy
+    void Flip()
+    {
+        // Filps the enemies scale to face the destination
+        if ((transform.position).x > destination.x && desiredScale.x > 0) // On right side
+        {
+            //transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            //desiredScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            desiredScale = new Vector3(desiredScale.x * -1, desiredScale.y, desiredScale.z);
+
+        }
+        if ((transform.position).x < destination.x && desiredScale.x < 0) // On left side
+        {
+            //transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            //desiredScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            desiredScale = new Vector3(desiredScale.x * -1, desiredScale.y, desiredScale.z);
+
+        }
     }
 }
