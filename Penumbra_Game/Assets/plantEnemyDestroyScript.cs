d7703b09316e84714b81e8e51d964371ae646566
Devastator1313 <113194162+Devastator1313@users.noreply.github.com>
2022-11-15 01:01:58 -0600

    bunch of stuff

---
+++ Penumbra_Game/Assets/plantEnemyDestroyScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plantEnemyDestroyScript : MonoBehaviour
+{
+    private float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Debug.Log("plant active: " + gameObject.activeInHierarchy);
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Drop Flame") || other.gameObject.CompareTag("Player"))
+        {
+            health -= 0.75f;
+            if (health <= 0)
+            {
+                gameObject.SetActive(false);
+
+            }
+        }
+    }
+}
