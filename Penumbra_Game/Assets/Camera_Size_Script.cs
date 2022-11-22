8ba1cdcf585a13a566f891851b249d54b446c0fb
Aidan <113620808+Gulfbones@users.noreply.github.com>
2022-11-22 08:58:04 -0600

    Add HP to canvas panel

---
+++ Penumbra_Game/Assets/Camera_Size_Script.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Skrypt odpowiada za usatwienie rozdzielczosci kemerze
+/// </summary>
+public class Camera_Size_Script : MonoBehaviour
+{
+
+
+    #region Pola
+    private int ScreenSizeX = 0;
+    private int ScreenSizeY = 0;
+    #endregion
+
+    #region metody
+
+    #region rescale camera
+    private void RescaleCamera()
+    {
+
+        if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
+
+        float targetaspect = 16.0f / 9.0f;
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+        float scaleheight = windowaspect / targetaspect;
+        Camera camera = GetComponent<Camera>();
+
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+
+        ScreenSizeX = Screen.width;
+        ScreenSizeY = Screen.height;
+    }
+    #endregion
+
+    #endregion
+
+    #region metody unity
+
+    void OnPreCull()
+    {
+        if (Application.isEditor) return;
+        Rect wp = Camera.main.rect;
+        Rect nr = new Rect(0, 0, 1, 1);
+
+        Camera.main.rect = nr;
+        GL.Clear(true, true, Color.black);
+
+        Camera.main.rect = wp;
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        float targetaspect = 16.0f / 9.0f;
+        // determine the game window's current aspect ratio
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+        // current viewport height should be scaled by this amount
+        float scaleheight = windowaspect / targetaspect;
+        // obtain camera component so we can modify its viewport
+        Camera camera = GetComponent<Camera>();
+        // if scaled height is less than current height, add letterbox
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+            camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+            Rect rect = camera.rect;
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+            camera.rect = rect;
+        }
+        /*
+        */
+        //RescaleCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RescaleCamera();
+    }
+    #endregion
+}
