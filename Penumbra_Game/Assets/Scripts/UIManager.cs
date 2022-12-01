848aed5947df901e1f71dd73ce33ef1c867fb98b
Devastator1313 <113194162+Devastator1313@users.noreply.github.com>
2022-12-01 09:48:18 -0600

    ui shenanigans

--- Penumbra_Game/Assets/Scripts/UIManager.cs
+++ Penumbra_Game/Assets/Scripts/UIManager.cs
@@ -28,12 +28,24 @@ public class UIManager : MonoBehaviour
         if (isPaused)
         {
             canvas.transform.GetChild(2).gameObject.SetActive(true);
+
+            canvas.transform.GetChild(0).gameObject.SetActive(false);
+            canvas.transform.GetChild(3).gameObject.SetActive(false);
+            canvas.transform.GetChild(4).gameObject.SetActive(false);
+            canvas.transform.GetChild(5).gameObject.SetActive(false);
+
+
             Time.timeScale = 0f;
         }
 
         else
         {
             canvas.transform.GetChild(2).gameObject.SetActive(false);
+
+            canvas.transform.GetChild(0).gameObject.SetActive(true);
+            canvas.transform.GetChild(3).gameObject.SetActive(true);
+            canvas.transform.GetChild(4).gameObject.SetActive(true);
+            canvas.transform.GetChild(5).gameObject.SetActive(true);
             Time.timeScale = 1;
         }
     }
