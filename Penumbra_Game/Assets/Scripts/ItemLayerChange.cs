80444731bcdb119688a6e637d1ed2caefbc65237
Atlas Collins <115568040+jackalyde@users.noreply.github.com>
2022-12-10 15:21:10 -0600

    Re-added item layer script and implemented it on all items in Cave Level
    GITHUB EATED MY CODE </3 so i redid it

---
+++ Penumbra_Game/Assets/Scripts/ItemLayerChange.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLayerChange : MonoBehaviour
+{
+    public SpriteRenderer currentSprite;
+    public GameObject player;
+    GameObject currentObject = null;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSprite = gameObject.GetComponent<SpriteRenderer>();
+
+        if (player.transform.position.y > currentSprite.transform.position.y)
+        {
+            currentSprite.sortingOrder = 12;
+            Debug.Log("Changed layer to 12");
+        }
+        if (player.transform.position.y < currentSprite.transform.position.y)
+        {
+            currentSprite.GetComponent<SpriteRenderer>().sortingOrder = 1;
+            Debug.Log("Changed layer to 1");
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            currentObject = other.gameObject;
+
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject == currentObject)
+            {
+                currentObject = null;
+            }
+        }
+
+
+    }
+}
+
