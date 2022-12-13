c95ef87790b1d8504bc49940b6adaaca4b87745b
Devastator1313 <113194162+Devastator1313@users.noreply.github.com>
2022-12-13 14:09:51 -0600

    working on implementing enemies into the farm level

--- Penumbra_Game/Assets/Scripts/Lantern_Checker.cs
+++ Penumbra_Game/Assets/Scripts/Lantern_Checker.cs
@@ -8,17 +8,30 @@ public class Lantern_Checker : MonoBehaviour
     public Light_Puzzle_Checker puzzle;
     public bool solved;
     public float timer;
+    public int numLit;
+    public GameObject plant1;
 
     void Start()
     {
         lanterns = gameObject.GetComponentsInChildren<lanternInteract>();
         puzzle = GameObject.Find("Light_Puzzle_Checker").gameObject.GetComponent<Light_Puzzle_Checker>();
         solved = false;
+        numLit = 0;
+        plant1 = GameObject.FindGameObjectWithTag("plant1");
+        plant1.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < lanterns.Length; i++)
+        {
+            if (lanterns[i].lit == true)
+            {
+                numLit++;
+            }
+        }
+        enableEnemies();
         timer += 1.0f * Time.deltaTime;
         if (solved == false && timer > 1.0f)
         {
@@ -47,6 +60,19 @@ public class Lantern_Checker : MonoBehaviour
         if (checking == true)
         {
             Destroy(GameObject.Find("ShadowWall"));
+        }
+    }
+
+    public void enableEnemies()
+    {
+        if (numLit > 3)
+        {
+            //enable enemy 1
+            plant1.SetActive(true);
+        }
+        if (numLit > 5)
+        {
+            //enable enemy 2
         }
     }
 
