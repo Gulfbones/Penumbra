c0c7b9389fbbd518511757fb9950352c0d1405cd
Devastator1313 <113194162+Devastator1313@users.noreply.github.com>
2022-12-14 16:49:38 -0600

    Revert "Fixed some scripts lantern"
    This reverts commit b331d544e42f606be9ecc3109c38aff5f5c5e64d.

--- Penumbra_Game/Assets/Scripts/Lantern_Checker.cs
+++ Penumbra_Game/Assets/Scripts/Lantern_Checker.cs
@@ -25,14 +25,34 @@ public class Lantern_Checker : MonoBehaviour
         //rat1.SetActive(false);
         plant2 = GameObject.FindGameObjectWithTag("plant2");
         plant2.SetActive(false);
-        
+        for (int i = 0; i < lanterns.Length; i++)
+        {
+            if (lanterns[i].lit == true)
+            {
+                numLit++;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+        numLit = 0;
+        for (int i = 0; i < lanterns.Length; i++)
+        {
+            if (lanterns[i].lit == true)
+            {
+                numLit++;
+            }
+        }
+        enableEnemies();
+        timer += 1.0f * Time.deltaTime;
+        if (solved == false && timer > 1.0f)
+        {
+            //Debug.Log("checked");
+            check();
+            timer = 0.0f;
+        }
     }
     /*private void FixedUpdate()
     { 
@@ -41,21 +61,16 @@ public class Lantern_Checker : MonoBehaviour
 
     public void check()
     {
-        numLit = 0;
+
         bool checking = true;
         for (int i = 0; i < lanterns.Length; ++i)
         {
-            if (lanterns[i].lit)
-            {
-                numLit++;
-            }
             if (lanterns[i].lit == false && puzzle.getSolved() == false)
             {
                 
                 checking = false;
             }
         }
-        enableEnemies();
         if (checking == true)
         {
             Destroy(GameObject.Find("ShadowWall"));
